6555a6101a1c9799a522d6641d02724aa871860e
Guilherme Dantas <guidanoli@hotmail.com>
2023-04-07 10:19:35 -0300

    fix: remove gas from Success and Failure

--- Bigstep.v
+++ Bigstep.v
@@ -20,8 +20,8 @@ Definition PEG : Type := list Exp.
 (* Parsing Result
    The result of parsing a string against a PEG *)
 Inductive Result : Type :=
-  | Success : string -> nat -> Result (* String suffix and remaining gas *)
-  | Failure : string -> nat -> Result (* String suffix and remaining gas *)
+  | Success : string -> Result (* String suffix *)
+  | Failure : string -> Result (* String suffix *)
   | OutOfGas : string -> Result (* String suffix *)
   | MissingRule : nat -> Result (* Terminal ID *)
   .
@@ -32,24 +32,24 @@ Fixpoint eparse (peg : PEG) (e : Exp) (s : string) (gas : nat) : Result :=
   match gas with
   | O => OutOfGas s
   | S gas' => match e with
-              | ETrue => Success s gas'
-              | EFalse => Failure s gas'
+              | ETrue => Success s
+              | EFalse => Failure s
               | ETerminal a => match s with
-                               | EmptyString => Failure s gas'
+                               | EmptyString => Failure s
                                | String a' s' => if Ascii.eqb a a'
-                                                 then Success s' gas'
-                                                 else Failure s gas'
+                                                 then Success s'
+                                                 else Failure s
                                end
               | ENonTerminal i => match nth_error peg i with
                                   | Some e' => eparse peg e' s gas'
                                   | None => MissingRule i
                                   end
               | ESequence e1 e2 => match eparse peg e1 s gas' with
-                                   | Success s' gas'' => eparse peg e2 s' gas'
+                                   | Success s' => eparse peg e2 s' gas'
                                    | res => res
                                    end
               | EOrderedChoice e1 e2 => match eparse peg e1 s gas' as res with
-                                        | Failure _ _ => eparse peg e2 s gas'
+                                        | Failure _ => eparse peg e2 s gas'
                                         | res => res
                                         end
               end

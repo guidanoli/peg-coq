41450495b30492a870a35ded4778a3d01dda6d1c
Guilherme Dantas <guidanoli@hotmail.com>
2023-04-10 00:24:36 -0300

    feat: add notation for if-then-else

--- BigstepInd.v
+++ BigstepInd.v
@@ -245,12 +245,14 @@ Notation "'&' A" := (EAndPredicate A) (at level 60, right associativity).
 Definition EIf (e1 e2 e3 : Exp) : Exp :=
   &e1; e2 // !e1; e3.
 
+Notation "'if&' A 'then' B 'else' C" := (EIf A B C) (at level 60, right associativity).
+
 (* If the condition is true, then
    the whole 'if-then-else' is equivalent to the 'then' *)
 Theorem if_true :
   forall peg e1 e2 e3 s s' res,
   parse peg e1 s (Success s') ->
-  (parse peg (EIf e1 e2 e3) s res <-> parse peg e2 s res).
+  (parse peg (if& e1 then e2 else e3) s res <-> parse peg e2 s res).
 Proof.
   intros.
   split; intros H'.
@@ -279,7 +281,7 @@ Qed.
 Theorem if_false :
   forall peg e1 e2 e3 s res,
   parse peg e1 s Failure ->
-  (parse peg (EIf e1 e2 e3) s res <-> parse peg e3 s res).
+  (parse peg (if& e1 then e2 else e3) s res <-> parse peg e3 s res).
 Proof.
   intros.
   split; intros H'.
@@ -302,10 +304,10 @@ Qed.
 Theorem if_undecided :
   forall peg e1 e2 e3 s,
   (forall res1, ~ parse peg e1 s res1) ->
-  (~ exists res2, parse peg (EIf e1 e2 e3) s res2).
+  (~ exists res2, parse peg (if& e1 then e2 else e3) s res2).
 Proof.
   intros peg e1 e2 e3 s H1 [res2 Hif].
-  parse_exp (EIf e1 e2 e3);
+  parse_exp (if& e1 then e2 else e3);
   parse_exp (&e1;e2);
   parse_exp (&e1);
   parse_exp (!e1);

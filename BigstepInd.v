1afa428d08cc384e71e2bb349ed6f0af71479404
Guilherme Dantas <guidanoli@hotmail.com>
2023-04-21 00:55:28 -0300

    feat!: dependent Exp and Grammar

--- BigstepInd.v
+++ BigstepInd.v
@@ -2,15 +2,17 @@ From Coq Require Import Strings.Ascii.
 From Coq Require Import Strings.String.
 From Coq Require Import Lists.List.
 From Coq Require Import Bool.Bool.
+From Peg Require Import Nth.
 Open Scope string.
 
-(* Parsing Expression *)
-Inductive Exp : Type :=
+(* Parsing Expression
+   Each expression can mention non-terminals i < n *)
+Inductive Exp {n : nat} : Type :=
   | ETrue : Exp (* Always matches *)
   | EFalse : Exp (* Never matches *)
   | EAny : Exp (* Matches any ASCII character *)
   | ETerminal : ascii -> Exp (* Matches an ASCII character *)
-  | ENonTerminal : nat -> Exp (* Matches a non-terminal *)
+  | ENonTerminal : forall i, i < n -> Exp (* Matches a non-terminal *)
   | ESequence : Exp -> Exp -> Exp (* Matches two subexpressions in sequence *)
   | EOrderedChoice : Exp -> Exp -> Exp (* Matches one of two subexpressions *)
   | ENotPredicate : Exp -> Exp (* Matches if subexpression doesn't *)
@@ -23,81 +25,110 @@ Notation "'!' A" := (ENotPredicate A) (at level 60, right associativity).
 Notation "A '~'" := (EKleeneStar A) (at level 60, right associativity).
 
 (* Parsing Expression Grammar
-   Each grammar is composed of a finite set of parsing rule *)
-Definition Grammar : Type := list Exp.
+   Each grammar is composed of n parsing rules *)
+Inductive Grammar (n : nat) : Type :=
+  | PEG : forall (l : list (@Exp n)), n = length l -> Grammar n.
+
+(* Get the i-th rule of a grammar of n rules, where i < n *)
+Definition rule {n : nat} (g : Grammar n) (i : nat) (Hi : i < n) : Exp :=
+  match g with
+  | PEG _ l Hlen => nth l i (PeanoNat.Nat.lt_stepr _ _ _ Hi Hlen)
+  end.
+
+Theorem rule_deterministic :
+  forall n (g : Grammar n) i H1 H2,
+  rule g i H1 = rule g i H2.
+Proof.
+  intros.
+  remember (rule g i H1) as e1.
+  remember (rule g i H2) as e2.
+  destruct g.
+  repeat match goal with
+  | Hx: _ = rule _ _ _ |- _ =>
+      unfold rule in Hx;
+      symmetry in Hx;
+      apply nth_equal in Hx
+  end.
+  match goal with
+  | Hx: ?a = Some _, Hy: ?a = Some _ |- _ =>
+      rewrite Hx in Hy;
+      injection Hy
+  end.
+  trivial.
+Qed.
 
 (* Match an expression
    Given a grammar, a parsing expression, and an input string,
    can either result in success (with `Some` leftover string)
    or failure (with `None` left) *)
-Inductive Match : Grammar -> Exp -> string -> option string -> Prop :=
+Inductive Match (n : nat) : Grammar n -> Exp -> string -> option string -> Prop :=
   | METrue :
       forall g s,
-      Match g ETrue s (Some s)
+      Match n g ETrue s (Some s)
   | MEFalse :
       forall g s,
-      Match g EFalse s None
+      Match n g EFalse s None
   | MEAny1 :
       forall g a s,
-      Match g EAny (String a s) (Some s)
+      Match n g EAny (String a s) (Some s)
   | MEAny2 :
       forall g,
-      Match g EAny EmptyString None
+      Match n g EAny EmptyString None
   | METerminal1 :
       forall g a s,
-      Match g (ETerminal a) (String a s) (Some s)
+      Match n g (ETerminal a) (String a s) (Some s)
   | METerminal2 :
       forall g a a' s,
       a <> a' ->
-      Match g (ETerminal a) (String a' s) None
+      Match n g (ETerminal a) (String a' s) None
   | METerminal3 :
       forall g a,
-      Match g (ETerminal a) EmptyString None
+      Match n g (ETerminal a) EmptyString None
   | MENonTerminal :
-      forall g i s e res,
-      nth_error g i = Some e ->
-      Match g e s res ->
-      Match g (ENonTerminal i) s res
+      forall g i s H res,
+      Match n g (rule g i H) s res ->
+      Match n g (ENonTerminal i H) s res
   | MESequence1 :
       forall g e1 e2 s s' res,
-      Match g e1 s (Some s') ->
-      Match g e2 s' res ->
-      Match g (e1; e2) s res
+      Match n g e1 s (Some s') ->
+      Match n g e2 s' res ->
+      Match n g (e1; e2) s res
   | MESequence2 :
       forall g e1 e2 s,
-      Match g e1 s None ->
-      Match g (e1; e2) s None
+      Match n g e1 s None ->
+      Match n g (e1; e2) s None
   | MEOrderedChoice1 :
       forall g e1 e2 s s',
-      Match g e1 s (Some s') ->
-      Match g (e1 // e2) s (Some s')
+      Match n g e1 s (Some s') ->
+      Match n g (e1 // e2) s (Some s')
   | MEOrderedChoice2 :
       forall g e1 e2 s res,
-      Match g e1 s None ->
-      Match g e2 s res ->
-      Match g (e1 // e2) s res
+      Match n g e1 s None ->
+      Match n g e2 s res ->
+      Match n g (e1 // e2) s res
   | MENotPredicate1 :
       forall g e s,
-      Match g e s None ->
-      Match g (!e) s (Some s)
+      Match n g e s None ->
+      Match n g (!e) s (Some s)
   | MENotPredicate2 :
       forall g e s s',
-      Match g e s (Some s') ->
-      Match g (!e) s None
+      Match n g e s (Some s') ->
+      Match n g (!e) s None
   | MEKleeneStar1 :
-      forall g e s s',
-      Match g (e; e~) s (Some s') ->
-      Match g (e~) s (Some s')
+      forall g e s s' res,
+      Match n g e s (Some s') ->
+      Match n g (e~) s' res ->
+      Match n g (e~) s res
   | MEKleeneStar2 :
       forall g e s,
-      Match g e s None ->
-      Match g (e~) s (Some s)
+      Match n g e s None ->
+      Match n g (e~) s (Some s)
   .
 
 (* Invert Match proposition with Exp e *)
 Ltac match_exp e :=
   match goal with
-  | H: Match _ e _ _ |- _ =>
+  | H: Match _ _ e _ _ |- _ =>
       inversion H; subst; eauto using Match
   end.
 
@@ -117,50 +148,55 @@ Ltac trivial_congruence :=
 (* Parsing a string against a parsing expression
    in the context of a grammar always outputs the same result *)
 Theorem match_deterministic :
-  forall g e s r1 r2,
-  Match g e s r1 ->
-  Match g e s r2 ->
+  forall n g e s r1 r2,
+  Match n g e s r1 ->
+  Match n g e s r2 ->
   r1 = r2.
 Proof.
-  intros g e s r1 r2 H1 H2.
+  intros n g e s r1 r2 H1 H2.
   generalize dependent r2.
   induction H1; intros r2 H';
   inversion H'; subst;
   try congruence;
   try trivial_congruence;
   try match goal with
-  [ IH: forall r, Match ?g (?e; ?e') ?s r -> Some _ = r,
-    H: Match ?g ?e ?s None |- _ ] =>
-        assert (Match g (e; e') s None);
+  [ IH: forall r, Match ?n ?g (?e; ?e') ?s r -> Some _ = r,
+    H: Match ?n ?g ?e ?s None |- _ ] =>
+        assert (Match n g (e; e') s None);
         eauto using Match
   end;
   try match goal with
-  [ IH: forall r, Match ?g ?e ?s r -> None = r,
-    H: Match g (?e; ?e') ?s (Some _) |- _ ] =>
+  [ IH: forall r, Match ?n ?g ?e ?s r -> None = r,
+    H: Match ?n g (?e; ?e') ?s (Some _) |- _ ] =>
         inversion H; subst
   end;
   try match goal with
-  [ IH: forall r, Match ?g ?e ?s r -> _ = r,
-    H: Match ?g ?e ?s _ |- _ ] =>
-      apply IH in H
+  [ IH: forall r, Match ?n ?g ?e ?s r -> _ = r,
+    H: Match ?n ?g ?e ?s _ |- _ ] =>
+        apply IH in H
+  end;
+  try match goal with
+  [ IH: forall rx, Match ?n ?g (rule ?g ?i ?H1) ?s rx -> ?r = rx,
+    H: Match ?n ?g (rule ?g ?i ?H2) ?s ?r' |- ?r = ?r' ] =>
+        erewrite rule_deterministic in H
   end;
   try discriminate;
   try match goal with
-    [ H: ?c ?e1 = ?c ?e2 |- _ ] =>
+  [ H: ?c ?e1 = ?c ?e2 |- _ ] =>
         assert (e1 = e2);
         try (congruence; trivial);
         clear H;
         subst
   end;
-  auto.
+  eauto.
 Qed.
 
 (* Use match_deterministic to discriminate
    different result types *)
 Ltac discriminate_results :=
   match goal with
-  [ H1: Match ?g ?e ?s (Some ?s'),
-    H2: Match ?g ?e ?s None |- _ ] =>
+  [ H1: Match ?n ?g ?e ?s (Some ?s'),
+    H2: Match ?n ?g ?e ?s None |- _ ] =>
         assert (Some s' = None);
         eauto using match_deterministic;
         discriminate
@@ -170,8 +206,8 @@ Ltac discriminate_results :=
    two successful (Some) results *)
 Ltac unify_results :=
    match goal with
-   [ H1: Match ?g ?e ?s (Some ?s1),
-     H2: Match ?g ?e ?s (Some ?s2) |- _ ] =>
+   [ H1: Match ?n ?g ?e ?s (Some ?s1),
+     H2: Match ?n ?g ?e ?s (Some ?s2) |- _ ] =>
          assert (Some s1 = Some s2) as Haux;
          eauto using match_deterministic;
          assert (s1 = s2);
@@ -183,14 +219,14 @@ Ltac unify_results :=
 (* Two parsing expressions are said equivalent in the
    context of some grammar iff for every input string,
    they output the same result *)
-Definition equivalent (g : Grammar) (e1 e2 : Exp) : Prop :=
+Definition equivalent (n : nat) (g : Grammar n) (e1 e2 : Exp) : Prop :=
   forall s res,
-  Match g e1 s res <-> Match g e2 s res.
+  Match n g e1 s res <-> Match n g e2 s res.
 
 (* Proving that the sequence expression is associative *)
 Theorem sequence_assoc :
-  forall g e1 e2 e3,
-  equivalent g (e1; (e2; e3))
+  forall n g e1 e2 e3,
+  equivalent n g (e1; (e2; e3))
                  ((e1; e2); e3).
 Proof.
   intros.
@@ -204,8 +240,8 @@ Qed.
 
 (* Proving that the ordered choice expression is associative *)
 Theorem ordered_choice_assoc :
-  forall g e1 e2 e3,
-  equivalent g (e1 // (e2 // e3))
+  forall n g e1 e2 e3,
+  equivalent n g (e1 // (e2 // e3))
                  ((e1 // e2) // e3).
 Proof.
   intros.
@@ -219,8 +255,8 @@ Qed.
 
 (* Show that a false first choice is useless *)
 Theorem first_choice_false :
-  forall g e,
-  equivalent g e (EFalse // e).
+  forall n g e,
+  equivalent n g e (EFalse // e).
 Proof.
   intros.
   constructor;
@@ -230,28 +266,28 @@ Proof.
   - (* <- *)
     inversion H; subst;
     eauto using Match;
-    match_exp EFalse.
+    match_exp (@EFalse n).
 Qed.
 
 (* Show that a true first choice is enough *)
 Theorem first_choice_true :
-  forall g e,
-  equivalent g ETrue (ETrue // e).
+  forall n g e,
+  equivalent n g ETrue (ETrue // e).
 Proof.
   intros.
   constructor;
   intros H.
   - (* -> *)
-    match_exp ETrue.
+    match_exp (@ETrue n).
   - (* <- *)
     match_exp (ETrue // e).
-    match_exp (ETrue).
+    match_exp (@ETrue n).
 Qed.
 
 (* Show that a false second choice is useless *)
 Theorem second_choice_false :
-  forall g e,
-  equivalent g e (e // EFalse).
+  forall n g e,
+  equivalent n g e (e // EFalse).
 Proof.
   intros.
   constructor;
@@ -261,38 +297,38 @@ Proof.
   - (* <- *)
     inversion H; subst; auto.
     destruct res; auto.
-    match_exp EFalse.
+    match_exp (@EFalse n).
 Qed.
 
 (* Show that a true first sequence part is useless *)
 Theorem first_part_true :
-  forall g e,
-  equivalent g e (ETrue; e).
+  forall n g e,
+  equivalent n g e (ETrue; e).
 Proof.
   intros.
   constructor;
   intros H;
   inversion H; subst;
   eauto using Match;
-  match_exp ETrue.
+  match_exp (@ETrue n).
 Qed.
 
 (* Show that a false first sequence part is enough *)
 Theorem first_part_false :
-  forall g e,
-  equivalent g EFalse (EFalse; e).
+  forall n g e,
+  equivalent n g EFalse (EFalse; e).
 Proof.
   intros.
   constructor;
   intros H;
   inversion H; subst;
   eauto using Match;
-  match_exp EFalse.
+  match_exp (@EFalse n).
 Qed.
 
 Theorem choice_sequence_distributive_left :
-  forall g e1 e2 e3,
-  equivalent g (e1; (e2 // e3))
+  forall n g e1 e2 e3,
+  equivalent n g (e1; (e2 // e3))
                  ((e1; e2) // (e1; e3)).
 Proof.
   intros.
@@ -318,8 +354,8 @@ Proof.
 Qed.
 
 Theorem result_suffix :
-  forall g e s res s2,
-  Match g e s res ->
+  forall n g e s res s2,
+  Match n g e s res ->
   res = Some s2 ->
   exists s1, s = s1 ++ s2.
 Proof.
@@ -361,13 +397,12 @@ Proof.
 Qed.
 
 (* And predicate *)
-Definition EAndPredicate (e : Exp) : Exp :=
-  !!e.
+Definition EAndPredicate {n : nat} (e : @Exp n) : Exp := !!e.
 
 Notation "'&' A" := (EAndPredicate A) (at level 60, right associativity).
 
 (* If-then-else expression *)
-Definition EIf (e1 e2 e3 : Exp) : Exp :=
+Definition EIf {n : nat} (e1 e2 e3 : @Exp n) : Exp :=
   &e1; e2 // !e1; e3.
 
 Notation "'if&' A 'then' B 'else' C" := (EIf A B C) (at level 60, right associativity).
@@ -375,21 +410,21 @@ Notation "'if&' A 'then' B 'else' C" := (EIf A B C) (at level 60, right associat
 (* If the condition is true, then
    the whole 'if-then-else' is equivalent to the 'then' *)
 Theorem if_condition_succeeds :
-  forall g e1 e2 e3 s s' res,
-  Match g e1 s (Some s') ->
-  Match g e2 s res ->
-  Match g (if& e1 then e2 else e3) s res.
+  forall n g e1 e2 e3 s s' res,
+  Match n g e1 s (Some s') ->
+  Match n g e2 s res ->
+  Match n g (if& e1 then e2 else e3) s res.
 Proof.
   intros.
-  assert (Match g (&e1) s (Some s));
+  assert (Match n g (&e1) s (Some s));
   destruct res;
   eauto using Match.
 Qed.
 
 Ltac contradict_match e :=
   match goal with
-  [ Hf: forall r, ~ Match _ e _ r,
-    Hx: Match _ e _ _ |- _ ] =>
+  [ Hf: forall r, ~ Match _ _ e _ r,
+    Hx: Match _ _ e _ _ |- _ ] =>
       apply Hf in Hx; contradiction
   end.
 
@@ -397,12 +432,12 @@ Ltac contradict_match e :=
    the 'then' expression is undecided, then
    the whole 'if-then-else' is undecided *)
 Theorem if_condition_succeeds_then_undec :
-  forall g e1 e2 e3 s s',
-  Match g e1 s (Some s') ->
-  (forall res, ~ Match g e2 s res) ->
-  (forall res, ~ Match g (if& e1 then e2 else e3) s res).
+  forall n g e1 e2 e3 s s',
+  Match n g e1 s (Some s') ->
+  (forall res, ~ Match n g e2 s res) ->
+  (forall res, ~ Match n g (if& e1 then e2 else e3) s res).
 Proof.
-  intros g e1 e2 e3 s s' H1 H2 r H3.
+  intros n g e1 e2 e3 s s' H1 H2 r H3.
   match_exp (if& e1 then e2 else e3);
   match_exp (&e1;e2);
   match_exp (&e1);
@@ -414,13 +449,13 @@ Qed.
 (* If the condition is false, then
    the whole 'if-then-else' is equivalent to the 'else' *)
 Theorem if_condition_fails :
-  forall g e1 e2 e3 s res,
-  Match g e1 s None ->
-  Match g e3 s res ->
-  Match g (if& e1 then e2 else e3) s res.
+  forall n g e1 e2 e3 s res,
+  Match n g e1 s None ->
+  Match n g e3 s res ->
+  Match n g (if& e1 then e2 else e3) s res.
 Proof.
   intros.
-  assert (Match g (&e1) s None);
+  assert (Match n g (&e1) s None);
   destruct res;
   eauto using Match.
 Qed.
@@ -429,12 +464,12 @@ Qed.
    the 'else' expression is undecided, then
    the whole 'if-then-else' is undecided *)
 Theorem if_condition_fails_else_undec :
-  forall g e1 e2 e3 s,
-  Match g e1 s None ->
-  (forall res, ~ Match g e3 s res) ->
-  (forall res, ~ Match g (if& e1 then e2 else e3) s res).
+  forall n g e1 e2 e3 s,
+  Match n g e1 s None ->
+  (forall res, ~ Match n g e3 s res) ->
+  (forall res, ~ Match n g (if& e1 then e2 else e3) s res).
 Proof.
-  intros g e1 e2 e3 s H1 H2 r H3.
+  intros n g e1 e2 e3 s H1 H2 r H3.
   match_exp (if& e1 then e2 else e3);
   match_exp (&e1;e2);
   match_exp (&e1);
@@ -449,11 +484,11 @@ Qed.
 (* If the condition is undecided, then
    the whole 'if-then-else' is undecided *)
 Theorem if_condition_undec :
-  forall g e1 e2 e3 s,
-  (forall res1, ~ Match g e1 s res1) ->
-  (forall res2, ~ Match g (if& e1 then e2 else e3) s res2).
+  forall n g e1 e2 e3 s,
+  (forall res1, ~ Match n g e1 s res1) ->
+  (forall res2, ~ Match n g (if& e1 then e2 else e3) s res2).
 Proof.
-  intros g e1 e2 e3 s H1 r H2.
+  intros n g e1 e2 e3 s H1 r H2.
   match_exp (if& e1 then e2 else e3);
   match_exp (&e1;e2);
   match_exp (&e1);
@@ -497,7 +532,7 @@ Reserved Notation "cs1 '<==[' e '@' g ']==' cs2" (at level 50).
          as if there expression `e` were in a sequence expression and this
          was the first of the following expression
 *)
-Inductive First : Grammar -> Exp -> charset -> charset -> Prop :=
+Inductive First (n : nat) : Grammar n -> @Exp n -> charset -> charset -> Prop :=
   | FETrue :
       forall g cs,
       cs <==[ ETrue @ g ]== cs
@@ -511,10 +546,9 @@ Inductive First : Grammar -> Exp -> charset -> charset -> Prop :=
       forall g cs a,
       singleton a <==[ ETerminal a @ g ]== cs
   | FENonTerminal :
-      forall g i e cs cs',
-      nth_error g i = Some e ->
-      cs <==[ e @ g ]== cs' ->
-      cs <==[ ENonTerminal i @ g ]== cs'
+      forall g i H cs cs',
+      cs <==[ rule g i H @ g ]== cs' ->
+      cs <==[ ENonTerminal i H @ g ]== cs'
   | FESequence :
       forall g cs cs' cs'' e1 e2,
       cs <==[ e1 @ g ]== cs' ->
@@ -534,23 +568,28 @@ Inductive First : Grammar -> Exp -> charset -> charset -> Prop :=
       cs <==[ e @ g ]== cs' ->
       union cs cs' <==[ e~ @ g ]== cs'
 
-where "cs1 '<==[' e '@' g ']==' cs2" := (First g e cs1 cs2).
+where "cs1 '<==[' e '@' g ']==' cs2" := (First _ g e cs1 cs2).
 
 Theorem first_deterministic :
-  forall g e cs1 cs2 cs',
+  forall n (g : Grammar n) e cs1 cs2 cs',
   cs1 <==[ e @ g ]== cs' ->
   cs2 <==[ e @ g ]== cs' ->
   cs1 = cs2.
 Proof.
-  intros g e cs1 cs2 cs' H1 H2.
+  intros n g e cs1 cs2 cs' H1 H2.
   generalize dependent cs2.
   induction H1; intros cs2' H2;
   inversion H2; subst;
   try trivial_congruence;
+  try match goal with
+  [ IH: forall csx, csx <==[ rule ?g ?i ?H1 @ ?g ]== ?cs' -> _ = csx,
+    H: _ <==[ rule ?g ?i ?H2 @ ?g ]== ?cs' |- _ ] =>
+        erewrite rule_deterministic in H
+  end;
   try repeat match goal with
   [ IH: forall csx, csx <==[ ?e @ ?g ]== ?cs' -> _ = csx,
     H: _ <==[ ?e @ ?g ]== ?cs' |- _ ] =>
         apply IH in H; subst
   end;
-  auto.
+  eauto.
 Qed.

df0fd90713756ab6793d8ae80724ec07f1079ad9
Guilherme Dantas <guidanoli@hotmail.com>
2023-05-31 23:21:22 -0300

    refactor: rewrite `WellFormed2.v`

--- WellFormed2.v
+++ WellFormed2.v
@@ -2,77 +2,62 @@
 
 From Coq Require Import Lia.
 From Coq Require Import Lists.List.
-From Coq Require Import Strings.String.
 
 (* Expression (stripped down) *)
 Inductive Exp : Type :=
   | ETrue : Exp (* Always matches *)
   | EAny : Exp (* Matches any ASCII character *)
-  | ENonTerminal : string -> Exp (* Matches a rule (labeled expression) *)
+  | ENonTerminal : nat -> Exp (* Matches a rule (indexed expression) *)
   | ESequence : Exp -> Exp -> Exp (* Matches two subexpressions in sequence *)
   .
 
 (* Cost of matching an expression *)
 Fixpoint cost (e : Exp) : nat :=
   match e with
-  | ETrue => 0
-  | EAny => 0
+  | ETrue => 1
+  | EAny => 1
   | ENonTerminal _ => 1
-  | ESequence e1 e2 => cost e1 + cost e2 + 1
+  | ESequence e1 e2 => S (cost e1 + cost e2)
   end.
 
-(* Map of string keys and values of type A *)
-Definition Map (A : Type) : Type := list (string * option A).
+(* A map is a list of index-value pairs *)
+Definition Map (A : Type) : Type := list (nat * A).
 
-(* Grammar, a finite set of rules *)
+(* Check if entry `e` has index `i` *)
+Definition hasIndex {A : Type} (i : nat) (e : nat * A) : bool :=
+  Nat.eqb i (fst e).
+
+(* Get the value of the first entry with index `i` *)
+Fixpoint get {A : Type} (map : Map A) (i : nat) : option A :=
+  match filter (hasIndex i) map with
+  | nil => None
+  | cons (_, a) _ => Some a
+  end.
+
+(* Delete every entry with index `i` *)
+Fixpoint del {A : Type} (map : Map A) (i : nat) : Map A :=
+  filter (fun e => negb (hasIndex i e)) map.
+
+(* Grammar, a finite set of indexed expressions *)
 Definition Grammar : Type := Map Exp.
 
-(* A character index counter for each rule *)
-Definition Counter : Type := Map nat.
-
-(* Retrieve a value from a map *)
-Fixpoint get {A : Type} (m : Map A) (l : string) : option A :=
-  match m with
-  | nil => None
-  | cons (l', a) m' => if eqb l l'
-                       then a
-                       else get m' l
-  end.
-
-(* Overwrite the value associated with a label in a map *)
-Definition set {A : Type} (m : Map A) (l : string) (a : A) : Map A :=
-  cons (l, Some a) m.
-
-(* Remove the value associated with a label in a map *)
-Definition unset {A : Type} (m : Map A) (l : string) : Map A :=
-  cons (l, None) m.
-
 Inductive Result : Type :=
-  | Wellformed : nat -> Result
-  | Malformed : Result
+  | WellFormed : bool -> Result (* Well-formed *)
+  | IllFormed : Result (* Ill-formed *)
   .
 
-Fixpoint wf (e : Exp) (g : Grammar) (c : Counter) (n : nat) (gas : nat) : option Result :=
+Fixpoint wf (e : Exp) (g : Grammar) (gas : nat) : option Result :=
   match gas with
   | O => None
   | S gas' => match e with
-              | ETrue => Some (Wellformed n)
-              | EAny => Some (Wellformed (S n))
-              | ENonTerminal l => match get g l with
-                                  (* Rule is not in grammar *)
-                                  | None => Some Malformed
-                                  (* Rule is in grammar *)
-                                  | Some e' => match get c l with
-                                               (* Rule was not visited *)
-                                               | None => wf e' g (set c l n) n gas'
-                                               (* Rule was visited *)
-                                               | Some n' => if Nat.eqb n n'
-                                                            then Some Malformed
-                                                            else Some (Wellformed n)
-                                               end
+              | ETrue => Some (WellFormed false)
+              | EAny => Some (WellFormed true)
+              | ENonTerminal i => match get g i with
+                                  | None => Some IllFormed
+                                  | Some e => wf e (del g i) gas'
                                   end
-              | ESequence e1 e2 => match wf e1 g c n gas' with
-                                   | Some (Wellformed n') => wf e2 g c n' gas'
+              | ESequence e1 e2 => match wf e1 g gas' with
+                                   | Some (WellFormed false) => wf e2 g gas'
                                    | res => res
                                    end
               end

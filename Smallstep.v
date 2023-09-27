2526a15aa2f252ded7175850a5da58eda0cb8380
Guilherme Dantas <guidanoli@proton.me>
2023-09-27 01:39:42 -0300

    feat: define multi and examples for step

--- Smallstep.v
+++ Smallstep.v
@@ -165,6 +165,125 @@ Proof.
   - apply final_is_nf.
 Qed.
 
+Inductive multi {X : Type} (R : relation X) : relation X :=
+  | multi_refl : forall (x : X), multi R x x
+  | multi_step : forall (x y z : X),
+                    R x y ->
+                    multi R y z ->
+                    multi R x z.
+
+Theorem multi_R : forall (X : Type) (R : relation X) (x y : X),
+  R x y -> (multi R) x y.
+Proof.
+  intros X R x y H.
+  apply multi_step with y.
+  - apply H.
+  - apply multi_refl.
+Qed.
+
+Theorem multi_trans :
+  forall (X : Type) (R : relation X) (x y z : X),
+  multi R x y ->
+  multi R y z ->
+  multi R x z.
+Proof.
+  intros X R x y z G H.
+  induction G.
+    - (* multi_refl *) assumption.
+    - (* multi_step *)
+      apply multi_step with y.
+      + assumption.
+      + apply IHG. assumption.
+Qed.
+
+Notation " t1 '-->*' t2 " := (multi step t1 t2) (at level 40).
+
+Ltac emulti_step :=
+  eapply multi_step; eauto using step.
+
+Ltac emulti :=
+  repeat (
+    try eapply multi_refl;
+    emulti_step
+  ).
+
+Module StepExamples.
+
+Example pat_a := PChar "a".
+
+Lemma pat_a_with_a :
+  (pat_a, "a"%string, nil) -->*
+  (PConst true, ""%string, nil).
+Proof.
+  emulti.
+Qed.
+
+Lemma pat_a_with_ab :
+  (pat_a, "ab"%string, nil) -->*
+  (PConst true, "b"%string, nil).
+Proof.
+  emulti.
+Qed.
+
+Lemma pat_a_with_b :
+  (pat_a, "b"%string, nil) -->*
+  (PConst false, "b"%string, nil).
+Proof.
+  emulti.
+  apply SChar2.
+  apply Ascii.eqb_neq.
+  trivial.
+Qed.
+
+Example pat_a_rep := PRepetition pat_a.
+
+Lemma pat_a_rep_with_empty :
+  (pat_a_rep, ""%string, nil) -->*
+  (PConst true, ""%string, nil).
+Proof.
+  emulti.
+Qed.
+
+Lemma pat_a_rep_with_a :
+  (pat_a_rep, "a"%string, nil) -->*
+  (PConst true, ""%string, nil).
+Proof.
+  emulti.
+Qed.
+
+Lemma pat_a_rep_with_aaa :
+  (pat_a_rep, "aaa"%string, nil) -->*
+  (PConst true, ""%string, nil).
+Proof.
+  emulti.
+Qed.
+
+Lemma pat_a_rep_with_xyz :
+  (pat_a_rep, "xyz"%string, nil) -->*
+  (PConst true, "xyz"%string, nil).
+Proof.
+  do 4 emulti_step.
+  {
+    apply SChar2.
+    intro.
+    discriminate.
+  }
+  emulti.
+Qed.
+
+Lemma pat_a_rep_with_aaron :
+  (pat_a_rep, "aaron"%string, nil) -->*
+  (PConst true, "ron"%string, nil).
+Proof.
+  do 14 emulti_step.
+  {
+    apply SChar2.
+    intro.
+    discriminate.
+  }
+  emulti.
+Qed.
+
 Inductive Result :=
   | Success : string -> Result
   | Failure : Result

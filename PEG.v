83f063c678f564d12df42be73f6e036a0bcdb371
Guilherme Dantas <guidanoli@proton.me>
2023-11-29 17:55:58 -0300

    feat: define suffix

--- PEG.v
+++ PEG.v
@@ -371,11 +371,49 @@ Ltac remember_match_subject name :=
         remember x as name
   end.
 
+(* Use this one instead of the other one,
+   and remove the other one *)
 Ltac remember_match_subject2 H name :=
   match goal with
     [ H: match ?x with | _ => _ end = _ |- _ ] =>
         remember x as name
   end.
+
+Inductive suffix : string -> string -> Prop :=
+  | SuffixRefl :
+      forall s,
+      suffix s s
+  | SuffixChar :
+      forall s1 s2 a,
+      suffix s1 s2 ->
+      suffix s1 (String a s2).
+
+Lemma suffix_correct :
+  forall s1 s2,
+  suffix s1 (append s2 s1).
+Proof.
+  intros.
+  induction s2.
+  - constructor.
+  - simpl.
+    constructor.
+    trivial.
+Qed.
+
+Lemma suffix_trans :
+  forall s1 s2 s3,
+  suffix s1 s2 ->
+  suffix s2 s3 ->
+  suffix s1 s3.
+Proof.
+  intros.
+  generalize dependent s2.
+  generalize dependent s1.
+  induction s3 as [|a s3 IH]; intros s1 s2 H12 H23;
+  inversion H23; subst; auto.
+  constructor.
+  eauto using IH.
+Qed.
 
 Theorem matches_comp_det :
   forall p s gas1 gas2 res1 res2,

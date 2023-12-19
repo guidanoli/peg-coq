d3d1a340539847c9058d3d9e2af4a0b5502b8970
Guilherme Dantas <guidanoli@proton.me>
2023-12-19 12:00:43 -0300

    feat: add grammars to matches predicate

--- PEG.v
+++ PEG.v
@@ -30,60 +30,61 @@ Inductive MatchResult : Type :=
   | Success : string -> MatchResult  (* Pattern matched and left string s.  *)
   .
 
-Inductive matches : pat -> string -> MatchResult -> Prop :=
+Inductive matches : list pat -> pat -> string -> MatchResult -> Prop :=
   | MEmptySuccess :
-      forall s,
-      matches PEmpty s (Success s)
+      forall g s,
+      matches g PEmpty s (Success s)
   | MCharSuccess :
-      forall a s,
-      matches (PChar a) (String a s) (Success s)
+      forall g a s,
+      matches g (PChar a) (String a s) (Success s)
   | MCharFailureEmptyString :
-      forall a,
-      matches (PChar a) EmptyString Failure
+      forall g a,
+      matches g (PChar a) EmptyString Failure
   | MCharFailureString :
-      forall a1 a2 s,
+      forall g a1 a2 s,
       a1 <> a2 ->
-      matches (PChar a1) (String a2 s) Failure
+      matches g (PChar a1) (String a2 s) Failure
   | MAnyCharSuccess :
-      forall a s,
-      matches PAnyChar (String a s) (Success s)
+      forall g a s,
+      matches g PAnyChar (String a s) (Success s)
   | MAnyCharFailure :
-      matches PAnyChar EmptyString Failure
+      forall g,
+      matches g PAnyChar EmptyString Failure
   | MSequenceSuccess :
-      forall p1 p2 s s' res,
-      matches p1 s (Success s') ->
-      matches p2 s' res ->
-      matches (PSequence p1 p2) s res
+      forall g p1 p2 s s' res,
+      matches g p1 s (Success s') ->
+      matches g p2 s' res ->
+      matches g (PSequence p1 p2) s res
   | MSequenceFailure :
-      forall p1 p2 s,
-      matches p1 s Failure ->
-      matches (PSequence p1 p2) s Failure
+      forall g p1 p2 s,
+      matches g p1 s Failure ->
+      matches g (PSequence p1 p2) s Failure
   | MChoiceSuccess :
-      forall p1 p2 s s',
-      matches p1 s (Success s') ->
-      matches (PChoice p1 p2) s (Success s')
+      forall g p1 p2 s s',
+      matches g p1 s (Success s') ->
+      matches g (PChoice p1 p2) s (Success s')
   | MChoiceFailure :
-      forall p1 p2 s res,
-      matches p1 s Failure ->
-      matches p2 s res ->
-      matches (PChoice p1 p2) s res
+      forall g p1 p2 s res,
+      matches g p1 s Failure ->
+      matches g p2 s res ->
+      matches g (PChoice p1 p2) s res
   | MRepetitionSuccess :
-      forall p s s' res,
-      matches p s (Success s') ->
-      matches (PRepetition p) s' res ->
-      matches (PRepetition p) s res
+      forall g p s s' res,
+      matches g p s (Success s') ->
+      matches g (PRepetition p) s' res ->
+      matches g (PRepetition p) s res
   | MRepetitionFailure :
-      forall p s,
-      matches p s Failure ->
-      matches (PRepetition p) s (Success s)
+      forall g p s,
+      matches g p s Failure ->
+      matches g (PRepetition p) s (Success s)
   | MNotSuccess :
-      forall p s s',
-      matches p s (Success s') ->
-      matches (PNot p) s Failure
+      forall g p s s',
+      matches g p s (Success s') ->
+      matches g (PNot p) s Failure
   | MNotFailure :
-      forall p s,
-      matches p s Failure ->
-      matches (PNot p) s (Success s)
+      forall g p s,
+      matches g p s Failure ->
+      matches g (PNot p) s (Success s)
   .
 
 Ltac destruct1 :=
@@ -94,8 +95,8 @@ Ltac destruct1 :=
 
 Ltac apply_matches_IH :=
   match goal with [
-    IHx: forall r, matches ?p ?s r -> _ = r,
-    Hx: matches ?p ?s _ |- _
+    IHx: forall r, matches ?g ?p ?s r -> _ = r,
+    Hx: matches ?g ?p ?s _ |- _
   ] =>
     apply IHx in Hx
   end.
@@ -103,12 +104,12 @@ Ltac apply_matches_IH :=
 (** Match predicate determinism **)
 
 Theorem matches_det :
-  forall p s res1 res2,
-  matches p s res1 ->
-  matches p s res2 ->
+  forall g p s res1 res2,
+  matches g p s res1 ->
+  matches g p s res2 ->
   res1 = res2.
 Proof.
-  intros p s res1 res2 H1 H2.
+  intros * H1 H2.
   generalize dependent res2.
   induction H1;
   intros res2 H2;
@@ -123,17 +124,17 @@ Qed.
 
 Ltac pose_matches_det :=
   match goal with
-  [ H1: matches ?p ?s ?r1,
-    H2: matches ?p ?s ?r2 |- _ ] =>
-        pose (matches_det p s r1 r2 H1 H2)
+  [ H1: matches ?g ?p ?s ?r1,
+    H2: matches ?g ?p ?s ?r2 |- _ ] =>
+        pose (matches_det g p s r1 r2 H1 H2)
   end.
 
 Example infinite_loop :
-  forall p s,
-  matches p s (Success s) ->
-  ~ (exists res, matches (PRepetition p) s res).
+  forall g p s,
+  matches g p s (Success s) ->
+  ~ (exists res, matches g (PRepetition p) s res).
 Proof.
-  intros p s H1 [res H2].
+  intros * H1 [res H2].
   remember (PRepetition p).
   induction H2;
   try destruct1;
@@ -146,8 +147,8 @@ Qed.
 (** Match suffix theorem **)
 
 Theorem matches_suffix :
-  forall p s s',
-  matches p s (Success s') ->
+  forall g p s s',
+  matches g p s (Success s') ->
   suffix s' s.
 Proof.
   intros.

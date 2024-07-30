fd5fda28046573188b08f212e46f27169dff48de
Guilherme Dantas <guidanoli@proton.me>
2024-07-30 11:12:09 -0300

    Move `pat_size` and `grammar_size` to beginning

--- PEG.v
+++ PEG.v
@@ -59,6 +59,48 @@ Inductive pat : Type :=
 
 Definition grammar : Type := list pat.
 
+(** Size **)
+(**********)
+
+Fixpoint pat_size p :=
+  match p with
+  | PEmpty => 1
+  | PChar _ => 1
+  | PAnyChar => 1
+  | PSequence p1 p2 => 1 + pat_size p1 + pat_size p2
+  | PChoice p1 p2 => 1 + pat_size p1 + pat_size p2
+  | PRepetition p => 1 + pat_size p
+  | PNot p => 1 + pat_size p
+  | PNT _ => 1
+  end.
+
+Fixpoint grammar_size g :=
+  match g with
+  | cons p g' => pat_size p + grammar_size g'
+  | nil => 0
+  end.
+
+Lemma pat_size_le_grammar_size :
+  forall p g,
+  In p g ->
+  pat_size p <= grammar_size g.
+Proof.
+  intros * HIn.
+  generalize dependent p.
+  induction g as [|p' g' IHg]; intros.
+  + (* nil *)
+    inversion HIn.
+  + (* cons p' g' *)
+    inversion HIn as [|HIn']; subst.
+    - (* p = p' *)
+      simpl.
+      lia.
+    - (* In p (p' :: g') *)
+      simpl.
+      specialize (IHg _ HIn').
+      lia.
+Qed.
+
 (** Semantics **)
 (***************)
 
@@ -2529,45 +2571,6 @@ Proof.
   intros * H Hle.
   induction Hle;
   auto using nullable_comp_S_gas.
-Qed.
-
-Fixpoint pat_size p :=
-  match p with
-  | PEmpty => 1
-  | PChar _ => 1
-  | PAnyChar => 1
-  | PSequence p1 p2 => 1 + pat_size p1 + pat_size p2
-  | PChoice p1 p2 => 1 + pat_size p1 + pat_size p2
-  | PRepetition p => 1 + pat_size p
-  | PNot p => 1 + pat_size p
-  | PNT _ => 1
-  end.
-
-Fixpoint grammar_size g :=
-  match g with
-  | cons p g' => pat_size p + grammar_size g'
-  | nil => 0
-  end.
-
-Lemma pat_size_le_grammar_size :
-  forall p g,
-  In p g ->
-  pat_size p <= grammar_size g.
-Proof.
-  intros * HIn.
-  generalize dependent p.
-  induction g as [|p' g' IHg]; intros.
-  + (* nil *)
-    inversion HIn.
-  + (* cons p' g' *)
-    inversion HIn as [|HIn']; subst.
-    - (* p = p' *)
-      simpl.
-      lia.
-    - (* In p (p' :: g') *)
-      simpl.
-      specialize (IHg _ HIn').
-      lia.
 Qed.
 
 (* Here, we could have used the max pat_size of the grammar

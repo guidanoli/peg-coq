c96400b391fe9dcc64f45e9235fbf2503b58d21b
Guilherme Dantas <guidanoli@proton.me>
2024-12-09 11:02:18 -0300

    Add First.v

---
+++ theories/First.v
@@ -0,0 +1,87 @@
+From Coq Require Import Lists.List.
+From Coq Require Import Strings.Ascii.
+From Coq Require Import Strings.String.
+From Peg Require Import Syntax.
+
+Import ListNotations.
+
+Definition charset : Type := (ascii -> bool).
+
+(* Charset with a single char *)
+Definition singlecharset a : charset :=
+  (fun b => (a =? b)%char).
+
+(* Union of charsets *)
+Definition unioncharset cs1 cs2 : charset :=
+  (fun a => orb (cs1 a) (cs2 a)).
+
+(* Set complement of a charset *)
+Definition complementcharset cs : charset :=
+  (fun a => negb (cs a)).
+
+Definition tocharset p : option charset :=
+  match p with
+  | PChar a => Some (singlecharset a)
+  | _ => None
+  end.
+
+Inductive first : grammar -> pat -> charset -> nat -> option (bool * charset) -> Prop :=
+  | FEmpty :
+      forall g cs d,
+      first g PEmpty cs d (Some (true, cs))
+  | FCharSet :
+      forall g p cs cs' d,
+      tocharset p = Some cs' ->
+      first g p cs d (Some (false, cs'))
+  | FSequenceNone :
+      forall g p1 p2 cs d,
+      first g p2 cs d None ->
+      first g (PSequence p1 p2) cs d None
+  | FSequenceSomeNone :
+      forall g p1 p2 cs cs2 d b2,
+      first g p2 cs d (Some (b2, cs2)) ->
+      first g p1 cs2 d None ->
+      first g (PSequence p1 p2) cs d None
+  | FSequenceSomeSome :
+      forall g p1 p2 cs cs2 cs1 d b2 b1,
+      first g p2 cs d (Some (b2, cs2)) ->
+      first g p1 cs2 d (Some (b1, cs1)) ->
+      first g (PSequence p1 p2) cs d (Some (andb b1 b2, cs1))
+  | FChoiceNone1 :
+      forall g p1 p2 cs d,
+      first g p1 cs d None ->
+      first g (PChoice p1 p2) cs d None
+  | FChoiceNone2 :
+      forall g p1 p2 cs d,
+      first g p2 cs d None ->
+      first g (PChoice p1 p2) cs d None
+  | FChoiceSome :
+      forall g p1 p2 cs cs1 cs2 d b1 b2,
+      first g p1 cs d (Some (b1, cs1)) ->
+      first g p2 cs d (Some (b2, cs2)) ->
+      first g (PChoice p1 p2) cs d (Some (orb b1 b2, unioncharset cs1 cs2))
+  | FRepetitionNone :
+      forall g p cs d,
+      first g p cs d None ->
+      first g (PRepetition p) cs d None
+  | FRepetitionSome :
+      forall g p cs d b cs',
+      first g p cs d (Some (b, cs')) ->
+      first g (PRepetition p) cs d (Some (true, unioncharset cs cs'))
+  | FNotNone :
+      forall g p cs d,
+      tocharset p = None ->
+      first g (PNot p) cs d (Some (true, cs))
+  | FNotSome :
+      forall g p cs cs' d,
+      tocharset p = Some cs' ->
+      first g (PNot p) cs d (Some (true, complementcharset cs'))
+  | FNTZero :
+      forall g i cs,
+      first g (PNT i) cs 0 None
+  | FNTSucc :
+      forall g i p cs d res,
+      nth_error g i = Some p ->
+      first g p cs d res ->
+      first g (PNT i) cs (S d) res
+  .
